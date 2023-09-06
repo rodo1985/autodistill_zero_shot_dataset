86083010052d6486b36ff880358b5bb81346793a
rodo1985 <rodo1985@gmail.com>
2023-09-06 08:27:49 +0000

    Added fiftyone

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 
 # Installation
 
+Follow the installation instruccions from [Fiftyone](https://docs.voxel51.com/index.html)
+
 Follow the installation instruccions from [GroundingDino](https://github.com/IDEA-Research/GroundingDINO)
 
 Follow the installation instruccions from [FastSAM](https://github.com/CASIA-IVA-Lab/FastSAM)
+
+
 
 ```bash
 
---
+++ train.py
@@ -0,0 +1,19 @@
+from ultralytics import YOLO
+import torch
+
+# paths
+log_dir = '/media/datasets/runs/detect'
+yaml_path = '/media/datasets/safety.yaml'
+
+# Select the device where we are going to train the model
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(device)
+
+# Define the base model
+model = YOLO('yolov8n.pt') 
+
+# train the model
+_ = model.train(data=yaml_path, epochs = 50, batch = 32)  
+
+# evaluate model performance on the validation set
+_ = model.val(data=yaml_path)
